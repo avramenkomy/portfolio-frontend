0420e6f5104f060f3b8ed5a3b8497c2d2467101c
avramenkomy <mihail.avramenko@ocrv.ru>
2022-12-10 20:44:08 +0300

    WIP: setup base routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import './App.css';
+
+import Home from './components/Home';
+import News from './components/News';
+import Blog from './components/Blog';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      Hello, world
-    </div>
+    <React.Fragment>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/news">News</Link>
+        <Link to="/blog">Blog</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </React.Fragment>
+    
   );
 }
 
