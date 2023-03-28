ec0c73cd199aa05279919365ab92a358f829eaee
avramenkomy <mihail.avramenko@ocrv.ru>
2023-03-28 22:29:04 +0300

    fix console.log

--- src/components/MiniProjects/ProjectCard.jsx
+++ src/components/MiniProjects/ProjectCard.jsx
@@ -4,7 +4,7 @@ import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 function ProjectCard ({ project_info }) {
-  console.log('project_info', project_info);
+  
   return (
     <Link to={`${project_info.url}`}>
       <div className="project__card">
